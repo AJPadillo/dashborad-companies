7ec30efb81f075fd39b759719075fc43899a55bc
AJPadillo <ajpadillo@gmail.com>
2025-01-27 11:29:21 +0100

    importado customicon a cardsummary

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,12 +1,15 @@
+import { CustomIcon } from "../CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 
 export function CardSummary(props: CardSummaryProps) {
-    const { average, icon, title, total, tooltipText } = props;
+    const { average, icon: Icon, title, tooltipText, total } = props;
     return (
         <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
             <div className="flex justify-between">
+                <CustomIcon icon={Icon}/>
                 {title}
             </div>
+            
         </div>
     )
 }
