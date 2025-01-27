c1eb22ee1a5033216f4d58c270edecdaf7ef18cc
AJPadillo <ajpadillo@gmail.com>
2025-01-27 10:28:19 +0100

    creado layout para personalizacion auth clerk

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LayoutAuth({ children }: {children: React.ReactNode}) {
+    return (
+        <div className="flex flex-col justify-center items-center h-full">
+            <p>AJPadillo Dashboard</p>
+            <h1 className="text-3xl my-2">Welcome to my Dashboard!</h1>
+            <h2 className="text-2xl mb-3">AJPadillo Dashboard</h2>
+            {children}
+        </div>
+    )
+}
