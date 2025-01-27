d1d262552ebaff68041d857be71a5f6f8fd82f36
AJPadillo <ajpadillo@gmail.com>
2025-01-27 12:00:37 +0100

    correccion visual de cardsumamry y agregado customtooltip al mismo

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,15 +1,20 @@
 import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
+import { CustomTooltip } from "@/components/CustomTooltip";
 
 export function CardSummary(props: CardSummaryProps) {
     const { average, icon: Icon, title, tooltipText, total } = props;
     return (
         <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
             <div className="flex justify-between">
-                <CustomIcon icon={Icon}/>
-                {title}
+                <div className="flex gap-2 items-center">
+                    <CustomIcon icon={Icon} />
+                    {title}
+                </div>
             </div>
-            
+            <CustomTooltip
+                content={tooltipText}
+            />
         </div>
     )
 }
