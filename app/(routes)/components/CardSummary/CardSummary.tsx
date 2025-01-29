cabef0f6f9de54776b6ec142039880249ea5565c
AJPadillo <ajpadillo@gmail.com>
2025-01-29 12:34:34 +0100

    agregados estilos para average con condicionales de porcentajes

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -2,6 +2,7 @@ import { CustomIcon } from "@/components/CustomIcon";
 import { CardSummaryProps } from "./CardSummary.types";
 import { CustomTooltip } from "@/components/CustomTooltip";
 import { cn } from "@/lib/utils";
+import { MoveDownRight, MoveUpRight, TrendingUp } from "lucide-react";
 
 export function CardSummary(props: CardSummaryProps) {
     const { average, icon: Icon, title, tooltipText, total } = props;
@@ -20,6 +21,15 @@ export function CardSummary(props: CardSummaryProps) {
                 <p className="text-2xl">{total}</p>
                 <div className={cn('flex items-center gap-1 px-2 text-xs text-white rounded-lg h-[20px] bg-black dark:bg-secondary')}>
                     {average}%
+                    {average < 20 && (
+                        <MoveDownRight strokeWidth={2} className="h-4 w-4" />
+                    )}
+                    {average > 20 && average < 70 && (
+                        <MoveUpRight strokeWidth={2} className="h-4 w-4" />
+                    )}
+                    {average > 70 && (
+                        <TrendingUp strokeWidth={2} className="h-4 w-4" />
+                    )}
                 </div>
             </div>
         </div>
