8586e8810345b90637f9484518b492f4ee40d28a
AJPadillo <ajpadillo@gmail.com>
2025-01-30 13:31:17 +0100

    agregado esquema basico 1 para navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,21 @@
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { UserButton } from "@clerk/nextjs"
+import { Menu } from "lucide-react"
 
 export function Navbar() {
     return (
-        <div>Navbar</div>
+        <div className="flex items-center px-2 gap-x-4 md:px-6 justify-between w-full bg-background border-b h-20">
+            <div className="block md:hidden">
+                <Sheet>
+                    <SheetTrigger className="flex items-center">
+                        <Menu />
+                    </SheetTrigger>
+                    <SheetContent side="left">
+                        <p>Sidebar Routes</p>
+                    </SheetContent>
+                </Sheet>
+            </div>
+        </div>
     )
 }
