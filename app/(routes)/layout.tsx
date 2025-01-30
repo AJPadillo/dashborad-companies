9e8865d5488cf823836f3add599d65a6c3a4a53b
AJPadillo <ajpadillo@gmail.com>
2025-01-30 13:05:40 +0100

    agregada estructura basica de layout dashboard

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,6 +1,16 @@
 
-export default function LayoutDashboard({children}: {children: React.ReactElement}) {
-  return (
-    <div>LayoutDashboard</div>
-  )
+export default function LayoutDashboard({ children }: { children: React.ReactElement }) {
+    return (
+        <div className="flex w-full h-full">
+            <div className="hidden xl:block w-80 h-full xl:fixed">
+                Sidebar
+            </div>
+            <div className="w-full xl:ml-80">
+                <p>Navbar</p>
+                <div className="p-6 bg-[#fafbfc] dark:bg-secondary">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
 }
