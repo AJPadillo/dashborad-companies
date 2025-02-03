4b75179b3919ba014e7eea58826c9fbe5f6a44d0
AJPadillo <ajpadillo@gmail.com>
2025-02-03 12:51:47 +0100

    importado datageneralsidebar a sidebar e iteracion elementos

--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,8 +1,16 @@
+import { dataGeneralSidebar, dataSupportSidebar, dataToolsSidebar } from './SidebarRoutes.data'
 
 export function SidebarRoutes() {
     return (
-        <div>
-            SidebarRoutes
+        <div className='flex flex-col justify-between h-full'>
+            <div>
+                <div className='p-2 md:p-6'>
+                    <p>GENERAL</p>
+                    {dataGeneralSidebar.map((item) => (
+                        <p key={item.label}>{item.label}</p>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
