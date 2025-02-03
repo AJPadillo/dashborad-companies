8158fa30517fef2479e3d41595ff731da2ea454a
AJPadillo <ajpadillo@gmail.com>
2025-02-03 13:10:19 +0100

    importado props y configurada estructura visual de sidebaritem

--- components/SidebarItem/SidebarItem.tsx
+++ components/SidebarItem/SidebarItem.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
+import { SidebarItemProps } from "./SidebarItem.types";
+import { cn } from "@/lib/utils";
 
-export function SidebarItem () {
+export function SidebarItem(props: SidebarItemProps) {
+    const { item } = props;
+    const { href, icon: Icon, label } = item;
+
     return (
-        <div>SidebarItem</div>
+        <Link href={href} className={cn('flex gap-x-2 nt-2 light:text-slate-700 dark:text-white text-sn items-center hover:bg-slate-300/20 p-2 rounded-lg cursor-pointer')}>
+            <Icon className="h-5 w-5" strokeWidth={1} />
+            {label}
+        </Link>
     )
 }
