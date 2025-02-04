d97b2b8195e060f163a76080510a987bf3374f94
AJPadillo <ajpadillo@gmail.com>
2025-02-04 12:53:03 +0100

    instalado theme-provicer de shadcn, creado archivo tsx e importado al layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Noto_Sans_Display } from "next/font/google";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
 
 const noto = Noto_Sans_Display({
@@ -22,7 +24,14 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={noto.className}>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
