bb6f62ba3ca430766cc31ba122ed3960c544bdbb
AJPadillo <ajpadillo@gmail.com>
2025-02-05 13:40:38 +0100

    agregado estilo visual a area en graphicsuscribers y modificados data de ejemplo

--- app/(routes)/components/GraphicSuscribers/GraphicSuscribers.data.ts
+++ app/(routes)/components/GraphicSuscribers/GraphicSuscribers.data.ts
@@ -6,42 +6,42 @@ export const dataGraphics = [
     },
     {
         year: "2018",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 5200,
+        oldCustomers: 3300
     },
     {
         year: "2019",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 8900,
+        oldCustomers: 5100
     },
     {
         year: "2020",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 7800,
+        oldCustomers: 3900
     },
     {
         year: "2021",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 7000,
+        oldCustomers: 3500
     },
     {
         year: "2022",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 4700,
+        oldCustomers: 2800
     },
     {
         year: "2023",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 5500,
+        oldCustomers: 3300
     },
     {
         year: "2024",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 9000,
+        oldCustomers: 5000
     },
     {
         year: "2025",
-        newCustomers: 4000,
-        oldCustomers: 2400
+        newCustomers: 8500,
+        oldCustomers: 4500
     },
 ]
