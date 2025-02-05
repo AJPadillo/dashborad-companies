719e277a87b9baab2f3758aa271de402b6efee1e
AJPadillo <ajpadillo@gmail.com>
2025-02-05 13:24:15 +0100

    creado data para graphicsuscribers

---
+++ app/(routes)/components/GraphicSuscribers/GraphicSuscribers.data.ts
@@ -0,0 +1,47 @@
+export const dataGraphics = [
+    {
+        year: "2017",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2018",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2019",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2020",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2021",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2022",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2023",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2024",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+    {
+        year: "2025",
+        newCustomers: 4000,
+        oldCustomers: 2400
+    },
+]
--- app/(routes)/components/GraphicSuscribers/GraphicSuscribers.tsx
+++ app/(routes)/components/GraphicSuscribers/GraphicSuscribers.tsx
@@ -1,6 +1,26 @@
+"use client";
 
-export function GraphicSuscribers () {
+import { TrendingUp } from "lucide-react";
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { dataGraphics } from "./GraphicSuscribers.data";
+
+export function GraphicSuscribers() {
     return (
-        <div>GraphicSuscribers</div>
+        <div className="mt-5">
+            <p className="text-3xl mb-3">26.441</p>
+            <div className="flex gap-x-5 mb-5">
+                <div className="flex items-center gap-2 px-3 text-md bg-[#16C8C7] text-white rounded-xl w-fit">
+                    3,7%
+                    <TrendingUp strokeWidth={1} className="h-4 w-4" />
+                </div>
+                <p className="text-slate-500">338 increased</p>
+            </div>
+            <div className="h-[350px]">
+        <ResponsiveContainer width="100%" height="100%">
+            <AreaChart width={730} height={250} data={dataGraphics} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+            </AreaChart>
+        </ResponsiveContainer>
+            </div>
+        </div>
     )
 }
