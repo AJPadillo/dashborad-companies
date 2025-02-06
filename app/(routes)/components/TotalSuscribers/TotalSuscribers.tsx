f4cbc37a4009a8674eac90a71f40312e985cc285
AJPadillo <ajpadillo@gmail.com>
2025-02-06 12:24:59 +0100

    añadida const data ejemplo a totalsuscribers.tsx

--- app/(routes)/components/TotalSuscribers/TotalSuscribers.tsx
+++ app/(routes)/components/TotalSuscribers/TotalSuscribers.tsx
@@ -1,6 +1,32 @@
+"use client";
+
+import { Percent } from "lucide-react";
+import { ResponsiveContainer, PieChart, Pie, Tooltip, Legend } from "recharts";
+
+import { CustomIcon } from "@/components/CustomIcon";
+
+const data = [
+    {
+        name: "Website",
+        value: 456,
+        fill: "#8884D8"
+    },
+    {
+        name: "Instagram",
+        value: 854,
+        fill: "#00C49F"
+    },
+    {
+        name: "Other",
+        value: 240,
+        fill: "#FFBB28"
+    }
+]
 
 export function TotalSuscribers() {
     return (
-        <div>TotalSusc</div>
+        <div>
+
+        </div>
     )
 }
