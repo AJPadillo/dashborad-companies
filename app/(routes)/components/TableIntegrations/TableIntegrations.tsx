27be8f22a703b3f5189609b11625bee6939a9b82
AJPadillo <ajpadillo@gmail.com>
2025-02-06 12:56:53 +0100

    importado codigo data-table de shadcn y creado archivo para props de tableintegrations

--- app/(routes)/components/TableIntegrations/TableIntegrations.tsx
+++ app/(routes)/components/TableIntegrations/TableIntegrations.tsx
@@ -1,6 +1,56 @@
+"use client"
+
+import * as React from "react"
+import { ArrowUpDown, ChevronUp, ChevronDown, MoreHorizontal } from "lucide-react"
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    SortingState,
+    VisibilityState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { TableIntegrationsProps } from "./TableIntegrations.types"
+
+
+const data: TableIntegrationsProps[] = [
+    {
+        app: "Stripe",
+        icon: "/images/stripe.png",
+        type: "Finance",
+        rate: 60,
+        profit: 450
+    }
+]
 
 export function TableIntegrations() {
     return (
-        <div>Table</div>
+        <div>
+
+        </div>
     )
 }
