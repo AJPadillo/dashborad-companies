213f4715011e502414ae4faca238964724f63ab4
AJPadillo <ajpadillo@gmail.com>
2025-02-06 12:34:00 +0100

    movido constante data a archivo data de totalsuscribers

--- app/(routes)/components/TotalSuscribers/TotalSuscribers.tsx
+++ app/(routes)/components/TotalSuscribers/TotalSuscribers.tsx
@@ -4,24 +4,8 @@ import { Percent } from "lucide-react";
 import { ResponsiveContainer, PieChart, Pie, Tooltip, Legend } from "recharts";
 
 import { CustomIcon } from "@/components/CustomIcon";
+import { dataTotalSuscribers } from "./TotalSuscribers.data";
 
-const data = [
-    {
-        name: "Website",
-        value: 456,
-        fill: "#8884D8"
-    },
-    {
-        name: "Instagram",
-        value: 854,
-        fill: "#00C49F"
-    },
-    {
-        name: "Other",
-        value: 240,
-        fill: "#FFBB28"
-    }
-]
 
 export function TotalSuscribers() {
     return (
@@ -33,7 +17,7 @@ export function TotalSuscribers() {
             <div className="w-full h-[200px] p-5">
                 <ResponsiveContainer aspect={1} maxHeight={200}>
                     <PieChart>
-                        <Pie data={data} dataKey="value" nameKey="name" labelLine={false} />
+                        <Pie data={dataTotalSuscribers} dataKey="value" nameKey="name" labelLine={false} />
                         <Tooltip />
                         <Legend />
                     </PieChart>
