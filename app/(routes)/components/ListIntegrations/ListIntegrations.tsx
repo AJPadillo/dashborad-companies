32f49262948e9e6b7e52d082d085a06d0f2e3228
AJPadillo <ajpadillo@gmail.com>
2025-02-07 12:43:55 +0100

    corregido y mejorado visual de listintegrations y tableintegrations

--- app/(routes)/components/ListIntegrations/ListIntegrations.tsx
+++ app/(routes)/components/ListIntegrations/ListIntegrations.tsx
@@ -4,7 +4,7 @@ import { TableIntegrations } from "../TableIntegrations"
 
 export function ListIntegrations() {
     return (
-        <div className="shadow-sm bg-background rounded-ls p-5 flex-1">
+        <div className="shadow-sm bg-background rounded-lg p-5 flex-1">
             <div className="flex gap-x-2 items-center">
                 <CustomIcon icon={List} />
                 <p className="text-xl">List of integrations</p>
