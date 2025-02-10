093afaafe513ecc994593f03533a8afadf6f89ea
AJPadillo <ajpadillo@gmail.com>
2025-02-10 12:50:11 +0100

    instalado componente dialog de shadcn, useState en headercompanies

--- app/(routes)/companies/components/HeaderCompanies/HeaderCompanies.tsx
+++ app/(routes)/companies/components/HeaderCompanies/HeaderCompanies.tsx
@@ -1,4 +1,21 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { CirclePlus } from "lucide-react"
+
+import { useState } from "react"
+
 export function HeaderCompanies() {
+    const [openModalCreate, setOpenModalCreate] = useState(false)
     return (
         <div>
             HeaderCompanies
