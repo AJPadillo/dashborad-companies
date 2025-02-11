bfe792db320a9d5ad25d21171099ce481031c572
AJPadillo <ajpadillo@gmail.com>
2025-02-11 13:08:03 +0100

    creado archivo types para formcreate y modificado esquema para creacion campos input

--- app/(routes)/companies/components/FormCreateCustomer/FormCreateCustomer.tsx
+++ app/(routes)/companies/components/FormCreateCustomer/FormCreateCustomer.tsx
@@ -15,42 +15,58 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { FormCreateCustomerProps } from "./FormCreateCustomer.types"
+import { useState } from "react"
 
 const formSchema = z.object({
-    username: z.string().min(2).max(50),
+    name: z.string(),
+    country: z.string().min(2),
+    website: z.string().min(2),
+    phone: z.string().min(6),
+    cif: z.string().min(6),
+    profileImage: z.string()
 })
 
-export function FormCreateCustomer() {
+export function FormCreateCustomer(props: FormCreateCustomerProps) {
+    const { setOpenModalCreate } = props
+    const [photoUploaded, setPhotoUploaded] = useState(false)
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: "",
+            name: "",
+            country: "",
+            website: "",
+            phone: "",
+            cif: "",
+            profileImage: "",
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    const { isValid } = form.formState
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values)
     }
     return (
         <div>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="shadcn" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                    <div className="grid grid-cols-2 gap-3">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Company name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Company name..." type="text" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
                     <Button type="submit">Submit</Button>
                 </form>
             </Form>
