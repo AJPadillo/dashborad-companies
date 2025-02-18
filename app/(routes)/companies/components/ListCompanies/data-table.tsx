316fbce1d1dd9767279cf2a4c66e8734496c34b5
AJPadillo <ajpadillo@gmail.com>
2025-02-18 14:02:58 +0100

    creado archivo data-table para listcompanies y agregadas importaciones

---
+++ app/(routes)/companies/components/ListCompanies/data-table.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from "react"
+
+import { ColumnDef, SortingState, flexRender, getCoreRowModel, ColumnFiltersState, getFilteredRowModel, getSortedRowModel, useReactTable, getPaginationRowModel } from '@tanstack/react-table'
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
