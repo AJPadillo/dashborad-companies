a823f198f00ed47034bc1631adba4911427b453c
AJPadillo <ajpadillo@gmail.com>
2025-02-18 13:57:08 +0100

    configurado listcompanies para obtencion datos de prisma

--- app/(routes)/companies/components/ListCompanies/ListCompanies.tsx
+++ app/(routes)/companies/components/ListCompanies/ListCompanies.tsx
@@ -1,5 +1,28 @@
+import { redirect } from 'next/navigation'
 
-export function ListCompanies() {
+import { auth } from '@clerk/nextjs'
+
+import { db } from '@/lib/db'
+
+export async function ListCompanies() {
+    const { userId } = auth()
+
+    if (!userId) {
+        return redirect('/')
+    }
+
+    const companies = await db.company.findMany({
+        where: {
+            userId
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    })
+
+    console.log(companies);
+    
+
     return (
         <div>ListCompanies</div>
     )
