42d1a839ce28abeca0c70d0227a58ea31d42b9c8
AJPadillo <ajpadillo@gmail.com>
2025-02-19 12:44:40 +0100

    agregada importacion de columns para datatable de listcompanies

--- app/(routes)/companies/components/ListCompanies/ListCompanies.tsx
+++ app/(routes)/companies/components/ListCompanies/ListCompanies.tsx
@@ -3,7 +3,9 @@ import { redirect } from 'next/navigation'
 import { auth } from '@clerk/nextjs'
 
 import { db } from '@/lib/db'
+
 import { DataTable } from './data-table'
+import { columns } from './columns'
 
 export async function ListCompanies() {
     const { userId } = auth()
