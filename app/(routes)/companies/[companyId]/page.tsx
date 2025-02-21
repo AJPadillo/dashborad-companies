8e94d48a3834a4c477a3c2acfc709e60105f7602
AJPadillo <ajpadillo@gmail.com>
2025-02-21 13:16:08 +0100

    agreada condicional de redireccion sobre autenticacion y companyId

--- app/(routes)/companies/[companyId]/page.tsx
+++ app/(routes)/companies/[companyId]/page.tsx
@@ -2,7 +2,25 @@ import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 
-export default function CompanyIdPage({ params }: { params: { companyId: string } }) {
+export default async function CompanyIdPage({ params }: { params: { companyId: string } }) {
+    const { userId } = auth()
+    if (!userId) {
+        return redirect("/")
+    }
+
+    const company = await db.company.findUnique(
+        {
+            where: {
+                id: params.companyId,
+                userId
+            }
+        }
+    )
+
+    if (!company) {
+        return redirect("/")
+    }
+
     return (
         <div>Page</div>
     )
