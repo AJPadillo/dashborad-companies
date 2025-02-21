f6eac363dfd7f4aee21275c90730c18a13a561d0
AJPadillo <ajpadillo@gmail.com>
2025-02-21 13:11:21 +0100

    agregada obtencion de params en CompanyIdPage de directorio dinamico companyId

--- app/(routes)/companies/[companyId]/page.tsx
+++ app/(routes)/companies/[companyId]/page.tsx
@@ -1,5 +1,8 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
-export default function CompanyIdPage() {
+export default function CompanyIdPage({ params }: { params: { companyId: string } }) {
     return (
         <div>Page</div>
     )
