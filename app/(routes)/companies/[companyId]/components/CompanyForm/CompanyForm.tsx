945a3835c4a76a6f27efeb5e591125c974aaaabc
AJPadillo <ajpadillo@gmail.com>
2025-02-24 12:27:14 +0100

    agregada importaciones de componentes a company form e instalado textarea de shadcn

--- app/(routes)/companies/[companyId]/components/CompanyForm/CompanyForm.tsx
+++ app/(routes)/companies/[companyId]/components/CompanyForm/CompanyForm.tsx
@@ -3,8 +3,17 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
+import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+
+import { Input } from "@/components/ui/input"
+import { Toast } from "@/components/ui/toast"
 
 import { CompanyFormProps } from "./CompanyForm.types"
 
