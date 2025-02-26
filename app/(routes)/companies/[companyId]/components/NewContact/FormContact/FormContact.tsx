560e60abe336e2cae1beaed2d0c1d5f4677cce8f
AJPadillo <ajpadillo@gmail.com>
2025-02-26 13:11:23 +0100

    creado archivo types de props para formcontact

--- app/(routes)/companies/[companyId]/components/NewContact/FormContact/FormContact.tsx
+++ app/(routes)/companies/[companyId]/components/NewContact/FormContact/FormContact.tsx
@@ -2,6 +2,8 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import axios from "axios"
+import { useParams, useRouter } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
@@ -9,10 +11,9 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Input } from "@/components/ui/input"
 import { Toast } from "@/components/ui/toast"
 
-import axios from "axios"
-import { useParams, useRouter } from "next/navigation"
+import { FormContactProps } from "./FormContact.types"
 
-export function FormContact() {
+export function FormContact(props: FormContactProps) {
     return (
         <div>FormContact</div>
     )
