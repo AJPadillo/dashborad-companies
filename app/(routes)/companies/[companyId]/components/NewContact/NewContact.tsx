a29423e9c8136085be7b0953a058f2a58ff7b908
AJPadillo <ajpadillo@gmail.com>
2025-02-26 12:56:45 +0100

    agregadas improtaciones Dialog en newcontact.tsx

--- app/(routes)/companies/[companyId]/components/NewContact/NewContact.tsx
+++ app/(routes)/companies/[companyId]/components/NewContact/NewContact.tsx
@@ -1,6 +1,17 @@
+"use client"
+
+import { useState } from "react"
+
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 
 export function NewContact() {
+    const [open, setOpen] = useState(false)
     return (
-        <div>NewContact</div>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button>Add new contact</Button>
+            </DialogTrigger>
+        </Dialog>
     )
 }
