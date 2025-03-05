9f11c95d349174b5d840c64f28f31d6ca491213d
AJPadillo <ajpadillo@gmail.com>
2025-03-05 13:12:50 +0100

    creada estructura de funcion POST en route api/contact

--- app/api/company/[companyId]/contact/route.ts
+++ app/api/company/[companyId]/contact/route.ts
@@ -1,3 +1,7 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+
+export async function POST(req: Request, {params}: {params: {companyId: string}}) {
+    
+}
