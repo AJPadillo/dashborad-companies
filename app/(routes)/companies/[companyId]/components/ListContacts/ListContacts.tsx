d166b879ba72b06f11fc84539afdf267c79357c6
AJPadillo <ajpadillo@gmail.com>
2025-03-19 12:23:42 +0100

    agregadas importaciones a listcontacts.tsx

--- app/(routes)/companies/[companyId]/components/ListContacts/ListContacts.tsx
+++ app/(routes)/companies/[companyId]/components/ListContacts/ListContacts.tsx
@@ -1,3 +1,11 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { Mail, Phone } from "lucide-react";
+
+import { Separator } from "@/components/ui/separator";
+
 import { ListContactsProps } from "./ListContacts.types";
 
 export function ListContacts(props: ListContactsProps) {
