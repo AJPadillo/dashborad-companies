cc3e38aeb98235dd3bc209e77f5fbf3d752e41a8
AJPadillo <ajpadillo@gmail.com>
2025-03-19 13:27:33 +0100

    agregadas importaciones a footercompany

--- app/(routes)/companies/[companyId]/components/FooterCompany/FooterCompany.tsx
+++ app/(routes)/companies/[companyId]/components/FooterCompany/FooterCompany.tsx
@@ -1,3 +1,12 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+import axios from "axios";
+
+import { Trash } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 import { FooterCompanyProps } from "./FooterCompany.types";
 
 export function FooterCompany(props: FooterCompanyProps) {
