5f395e1b841b04d0f72b9ea3e5661b54e0a83f72
AJPadillo <ajpadillo@gmail.com>
2025-03-25 12:04:28 +0100

    agregadas importaciones calendar a calendar.tsx

--- app/(routes)/tasks/components/Calendar/Calendar.tsx
+++ app/(routes)/tasks/components/Calendar/Calendar.tsx
@@ -1,3 +1,20 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+
+import multiMonth from '@fullcalendar/multimonth'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+import { DateSelectArg, EventContentArg } from '@fullcalendar/core/index.js'
+
+import axios from 'axios';
+
+
+
 import { CalendarProps } from "./Calendar.types";
 
 export function Calendar(props: CalendarProps) {
